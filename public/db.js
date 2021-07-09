715b88a22ecaef683d8f09b3b71eb02e63b954b0
Hanny Biggsmen <hannah.biggam@gmail.com>
2021-07-09 14:37:48 +0200

    adding db.js; should function correctly. has ntoes

--- public/db.js
+++ public/db.js
@@ -1,1 +1,68 @@
-// Creates a db request for database
+let db;
+const request = indexedDB.open("budget", 1);
+
+
+// creates "pending" object which autoIncrements
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
+
+
+// checks if online, if not throws error
+request.onsuccess = function (event) {
+  db = event.target.result;
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+request.onerror = function (event) {
+  console.log("Error!" + event.target.errorCode);
+};
+
+
+// saves record
+function saveRecord(record) {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+
+  store.add(record);
+}
+
+
+// checks database and fetches
+function checkDatabase() {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+        .then(response => response.json())
+        .then(() => {
+          const transaction = db.transaction(["pending"], "readwrite");
+          const store = transaction.objectStore("pending");
+          store.clear();
+        });
+    }
+  };
+}
+
+function deletePending() {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  store.clear();
+}
+
+
+
+// when online, check database function above runs
+window.addEventListener("online", checkDatabase);
